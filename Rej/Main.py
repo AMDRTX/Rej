b670236ad35eec5476b090b323da96d5351ce04b
validator <73761107+AMDRTX@users.noreply.github.com>
2021-05-23 09:42:27 +0100

    Add files via upload

---
+++ Rej/Main.py
@@ -0,0 +1,34 @@
+
+print("""\033[1;35;40m 
+██████╗░███████╗░░░░░██╗
+██╔══██╗██╔════╝░░░░░██║
+██████╔╝█████╗░░░░░░░██║
+██╔══██╗██╔══╝░░██╗░░██║
+██║░░██║███████╗╚█████╔╝
+╚═╝░░╚═╝╚══════╝░╚════╝░  \n""")
+
+install = input('URL:')
+filetype = input('FileType:')
+epic = install.find('q , w , e , r , t , y , u , i , o , p , a , s , d , f , g , h , j , k , l , z , x , c , v , b , n , m')
+import urllib.request
+import requests
+import random
+blood = ('1 2 3 4 5 6 7 8 9')
+twa = random.choice(blood)
+sap = random.choice(blood)
+one = random.choice(blood)
+two = random.choice(blood)
+three = random.choice(blood)
+dc4 = random.choice(blood)
+nuhap = random.choice(blood)
+import os
+user = os.getlogin()
+import urllib.request
+url = install
+h = len(install)
+try:
+    mile = urllib.request.urlretrieve(url, f'C:/Users/{user}/Downloads/{h}{one}{two}{dc4}{twa}{nuhap}{sap}.{filetype}')
+except:
+    print("Sorry We Could not find that | |")
+    print("                             ----")
+print(f' Saved File In  C:/Users/{user}/Downloads/{h}{one}{two}{dc4}{twa}{nuhap}{sap}.{filetype} ')
